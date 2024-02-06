b321569943b73124a5037668a7541adab69e2e5b
AN CHARAN <chanducharan529@gmail.com>
2024-02-06 12:04:19 +0530

    Changes in Database and Validation

--- database.js
+++ database.js
@@ -1,11 +1,52 @@
 const mongoose=require("mongoose");
 mongoose.connect("mongodb+srv://ankith1703:8IKldrs7uxnFQhHK@cluster0.5ukccla.mongodb.net/")
+
 const schemaofentryindatabase=mongoose.Schema({
-    title:String
+    name:String,
+    email:String,
+    mcq1:Number,
+    mcq2:Number,
+    mcq3:Number,
+    mcq4:Number,
+    mcq5:Number,
+    mcq6:Number,
+    interestedPeople: [{ type: String, ref: 'entry' }], // Assuming username of other entries
+    connectedUsers: [{ type: String, ref: 'entry' }],
+    // interestedPeople: [{ type: String }],
+    // connectedUsers: [{ type: String }]
 });
-const table_entitytoacesssentiredata=mongoose.model('entry',schemaofentryindatabase)
+
+
+const matchResultSchema = new mongoose.Schema({
+    userName: String,
+    matchedUserName: String,
+    matchPercentage: Number,
+  });
+  
+  const mutualFriendsSchema = new mongoose.Schema({
+    user1: {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'entry' },
+        userName: String,
+      },
+      user2: {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'entry' },
+        userName: String,
+      }
+  });
+  
+  
+const table_entitytoacesssentiredata=mongoose.model('entry',schemaofentryindatabase);
+
+const MatchResult = mongoose.model('matchResult', matchResultSchema);
+
+const MutualFriends = mongoose.model('MutualFriends', mutualFriendsSchema);
 
 module.exports=
 {
-    table_entitytoacesssentiredata:table_entitytoacesssentiredata
+    table_entitytoacesssentiredata:table_entitytoacesssentiredata,
+    MatchResult:MatchResult,
+    MutualFriends:MutualFriends
 }
+
+
+
--- zod.js
+++ zod.js
@@ -1,7 +1,16 @@
 const zod=require("zod");
 const genericbodytypetobereceivedfromuser=zod.object(
         {
-            title:zod.string()
+            name:zod.string(),
+            email:zod.string().email(),
+            mcq1:zod.number(),
+            mcq2:zod.number(),
+            mcq3:zod.number(),
+            mcq4:zod.number(),
+            mcq5:zod.number(),
+            mcq6:zod.number(),
+            interestedPeople: zod.array(zod.string()),
+            connectedUsers: zod.array(zod.string())
         }
     )
 
